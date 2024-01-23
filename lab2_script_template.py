df36dbb9c1e3616b092db8a9526e80199d3ee886
Yimbeau2 <156677831+Yimbeau2@users.noreply.github.com>
2024-01-23 18:13:18 -0500

    Update lab2_script_template.py
    - Completed about_me dictionary
    - Started print function
    - Started add_pizza_toppings funciton

--- lab2_script_template.py
+++ lab2_script_template.py
@@ -2,24 +2,57 @@ def main():
 
     # TODO: Step 2 - Create a complex data structure
     about_me = {
-    'full_name': 'Jonathan', 'Sproule'
-    'student_id': '69696969699'
-    'pizza_toppings' { 'PINEAPPLE', 'CHEESE', 'PEPPERS' }  
-    'movies': [ ]
 
+    'full_name': 'Jonathan Sproule',
 
+    'student_id': '69696969699',
+
+    'pizza_toppings': { 'PINEAPPLE', 'CHEESE', 'PEPPERS' }, 
+
+    'movies': [
+
+            {'title': ' Harry Potter',
+             
+            'genre': ' fantasy'},
+            
+            {'title': 'Ferrari vs Ford',
+             
+            'genre': 'drama doc'}
+            ]
     }
 
+    # Calling First print statment function
+    print_student_name_and_id(about_me)
+
+
+    # Calling function to add more pizza toppings
+    add_pizza_toppings(about_me,[toppings: 'ONIONS', 'EXTRA CHEESE', 'BACON'] ) 
+
+
     # TODO: Step 3 - Add another movie to the data structure
-    
+
+    about_me['movies'] = {'title': 'Fantastic Beasts And Where To Find Them'}
+
+
 # TODO: Step 4 - Function that prints student name and ID	
 def print_student_name_and_id(about_me):
-    print(f"My name is {full_name}, but you can call me sir {first_name}")
-    print(f"My student ID is {student_id}")
+
+    print(f"My name is {about_me: [full_name]}, but you can call me Lord {about_me: [full_name.split()]}")
+
+    print(f"My student ID is {about_me:[student_id]}")
     return
     
 # TODO: Step 5 - Function that adds pizza toppings to data structure
 def add_pizza_toppings(about_me, toppings):
+
+    about_me[toppings] = [toppings.split()]
+
+    about_me[toppings] = [list.sort([toppings])]
+
+    about_me[toppings] = [toppings.lower()]
+
+    about_me[toppings] = [toppings.append(toppings)]
+
     return
 
 # TODO: Step 6 - Function that prints bullet list of pizza toppings
